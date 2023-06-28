077fd4d3c887e7bb919ccb18d85f72e66362669b
aliesmaelfard <aliesmaelfard@gmail.com>
2023-06-29 00:18:47 +0330

    Add project files.

---
+++ Maui.CV/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.CV;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Maui.CV/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Maui.CV;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+     
+    }
+
+
+    protected override void OnAppearing()
+    {
+		
+	
+        base.OnAppearing();
+    }
+}
+
